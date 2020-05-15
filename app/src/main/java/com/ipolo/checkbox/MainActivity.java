755c6624adeac773c29dcc497b068b239549a692
IlyaPolonskiy <mr.polonskiy96@mail.ru>
2020-05-15 21:19:34 +0300

    Initial commit

---
+++ app/src/main/java/com/ipolo/checkbox/MainActivity.java
@@ -0,0 +1,89 @@
+package com.ipolo.checkbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mInputMoney;
+    private EditText mInputInfo;
+    private Button mBtnOk;
+    private CheckBox mBankCardChkBx;
+    private CheckBox mMobilePhoneChkBx;
+    private CheckBox mCashAddressChkBx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews(){
+        mInputMoney = findViewById(R.id.inputMoney);
+        mInputInfo = findViewById(R.id.inputInfo);
+        mBtnOk = findViewById(R.id.btnOK);
+        mBankCardChkBx = findViewById(R.id.bankCardChkBx);
+        mMobilePhoneChkBx = findViewById(R.id.mobilePhoneChkBx);
+        mCashAddressChkBx = findViewById(R.id.cashAddressChkBx);
+        mBankCardChkBx.setOnCheckedChangeListener(checkedChangeListener);
+        mMobilePhoneChkBx.setOnCheckedChangeListener(checkedChangeListener);
+        mCashAddressChkBx.setOnCheckedChangeListener(checkedChangeListener);
+
+        mBtnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, payInfo(mInputMoney, mInputInfo), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private String payInfo(EditText mInputMoney, EditText mInputInfo){
+        if (mInputMoney.getText().length() == 0 || mInputInfo.getText().length() == 0){
+            return "Сумма: 0\n" + "Оплата: Не выбрано";
+        } else {
+            return "Сумма: " + mInputMoney.getText().toString() +
+                    "\nОплата: " + mInputInfo.getText().toString();
+        }
+    }
+
+    private void resetCheckBoxes(){
+        mBankCardChkBx.setChecked(false);
+        mMobilePhoneChkBx.setChecked(false);
+        mCashAddressChkBx.setChecked(false);
+    }
+
+    CompoundButton.OnCheckedChangeListener checkedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked){
+                switch (buttonView.getId()){
+                    case R.id.bankCardChkBx:
+                        resetCheckBoxes();
+                        mBankCardChkBx.setChecked(true);
+                        mInputInfo.setInputType(InputType.TYPE_CLASS_NUMBER);
+                        break;
+                    case R.id.mobilePhoneChkBx:
+                        resetCheckBoxes();
+                        mMobilePhoneChkBx.setChecked(true);
+                        mInputInfo.setInputType(InputType.TYPE_CLASS_PHONE);
+                        break;
+                    case R.id.cashAddressChkBx:
+                        resetCheckBoxes();
+                        mCashAddressChkBx.setChecked(true);
+                        mInputInfo.setInputType(InputType.TYPE_CLASS_TEXT);
+                        break;
+                    default:
+                }
+            }
+        }
+    };
+}
